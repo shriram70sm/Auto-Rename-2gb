2a0f5d10de1c785eed772a15f8d24de08d7fc4f5
shriram70sm <shriramkulkarni057@gmail.com>
2026-01-13 18:04:58 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -70,7 +70,7 @@ class Bot(Client):
                         f"ɪ ᴅɪᴅɴ'ᴛ sʟᴇᴘᴛ sɪɴᴄᴇ​: `{uptime_string}`"
                     ),
                     reply_markup=InlineKeyboardMarkup(
-                        [[InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇs", url="https://t.me/RexBots_Official")]]
+                        [[InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇs", url="https://t.me/MovieGalaxyX")]]
                     )
                 )
             except Exception as e:
