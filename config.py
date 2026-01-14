d04a040d6cd8620f26ef8e8437920b35d68aa73e
shriram70sm <shriramkulkarni057@gmail.com>
2026-01-14 15:38:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ class Config(object):
 
     # database config
     DB_NAME = os.environ.get("DB_NAME", "MovieGalaxyX")     
-    DB_URL  = os.environ.get("DB_URL", "mongodb+srv://MovieGalaxyX:MovieGalaxyX@moviegalaxyx.5l324s1.mongodb.net/?appName=MovieGalaxyX")
+    DB_URL  = os.environ.get("DB_URL", "")
  
     # other configs
     ADMIN_URL = "https://t.me/MovieGalaxyX"
